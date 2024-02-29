16a13908c1336f585d2e8604312d626928ba1ff1
Gayathri <142381512+gaya3-zipstack@users.noreply.github.com>
2024-02-29 15:14:44 +0530

    Fix file upload security loopholes (#14)
    * Add file upload validation
    
    * Code formatting
    
    * Code formatting
    
    * Change values to constants

--- backend/file_management/serializer.py
+++ backend/file_management/serializer.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
+from file_management.constants import FileInformationKey
+from utils.FileValidator import FileValidator
+
 
 class FileInfoSerializer(serializers.Serializer):
     name = serializers.CharField()
@@ -15,13 +18,36 @@ class FileListRequestSerializer(serializers.Serializer):
 
 
 class FileUploadSerializer(serializers.Serializer):
-    file = serializers.ListField(child=serializers.FileField(), required=True)
+    file = serializers.ListField(
+        child=serializers.FileField(),
+        required=True,
+        validators=[
+            FileValidator(
+                allowed_extensions=FileInformationKey.FILE_UPLOAD_ALLOWED_EXTENSIONS,
+                allowed_mimetypes=FileInformationKey.FILE_UPLOAD_ALLOWED_MIMETYPES,
+                min_size=0,
+                max_size=FileInformationKey.FILE_UPLOAD_MAX_SIZE,
+            )
+        ],
+    )
+    # FileExtensionValidator(allowed_extensions=['pdf'])
     connector_id = serializers.UUIDField()
     path = serializers.CharField()
 
 
 class FileUploadIdeSerializer(serializers.Serializer):
-    file = serializers.ListField(child=serializers.FileField(), required=True)
+    file = serializers.ListField(
+        child=serializers.FileField(),
+        required=True,
+        validators=[
+            FileValidator(
+                allowed_extensions=FileInformationKey.FILE_UPLOAD_ALLOWED_EXTENSIONS,
+                allowed_mimetypes=FileInformationKey.FILE_UPLOAD_ALLOWED_MIMETYPES,
+                min_size=0,
+                max_size=FileInformationKey.FILE_UPLOAD_MAX_SIZE,
+            )
+        ],
+    )
 
 
 class FileInfoIdeSerializer(serializers.Serializer):
