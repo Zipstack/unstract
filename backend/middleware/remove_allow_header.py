874a635c7cb896620f8bda2581a8e7b205ba8559
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2024-03-04 10:33:45 +0530

    Fixed import issue

--- backend/middleware/remove_allow_header.py
+++ backend/middleware/remove_allow_header.py
@@ -1,8 +1,21 @@
+from functools import wraps
 
-def RemoveAllowHeaderMiddleware(view_func):
-    def wrapper(*args, **kwargs):
-        response = view_func(*args, **kwargs)
+
+class RemoveAllowHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
         if 'Allow' in response:
             del response['Allow']
         return response
-    return wrapper
+
+    def remove_allow_header(self):
+        @wraps(self)
+        def wrapper(*args, **kwargs):
+            response = self(*args, **kwargs)
+            if 'Allow' in response:
+                del response['Allow']
+            return response
+        return wrapper
