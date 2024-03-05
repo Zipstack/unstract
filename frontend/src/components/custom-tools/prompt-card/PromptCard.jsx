86ff1bb171535b40792fd3b9036c158a335ec1fe
Tahier Hussain <89440263+tahierhussain@users.noreply.github.com>
2024-03-05 13:15:02 +0530

    Render Components Only If Available (#57)
    * Render component only when available
    
    * Handle the usage of plugin components in the FE
    
    * Updated .gitignore
    
    * Minor fix
    
    * Moved the eval related functions to the plugins folder
    
    ---------
    
    Co-authored-by: Hari John Kuriakose <hari@zipstack.com>

--- frontend/src/components/custom-tools/prompt-card/PromptCard.jsx
+++ frontend/src/components/custom-tools/prompt-card/PromptCard.jsx
@@ -43,10 +43,30 @@ import { ConfirmModal } from "../../widgets/confirm-modal/ConfirmModal";
 import SpaceWrapper from "../../widgets/space-wrapper/SpaceWrapper";
 import { SpinnerLoader } from "../../widgets/spinner-loader/SpinnerLoader";
 import { EditableText } from "../editable-text/EditableText";
-import { EvalMetricTag } from "../eval-metric-tag/EvalMetricTag";
-import { EvalModal } from "../eval-modal/EvalModal";
 import { OutputForDocModal } from "../output-for-doc-modal/OutputForDocModal";
 import "./PromptCard.css";
+
+let EvalBtn = null;
+let EvalMetrics = null;
+let EvalModal = null;
+let sortEvalMetricsByType = (param) => {
+  return [];
+};
+let getEvalMetrics = (param1, param2, param3) => {
+  return [];
+};
+try {
+  EvalBtn = require("../../../plugins/eval-btn/EvalBtn").EvalBtn;
+  EvalMetrics =
+    require("../../../plugins/eval-metrics/EvalMetrics").EvalMetrics;
+  EvalModal = require("../../../plugins/eval-modal/EvalModal").EvalModal;
+  sortEvalMetricsByType =
+    require("../../../plugins/eval-helper/EvalHelper").sortEvalMetricsByType;
+  getEvalMetrics =
+    require("../../../plugins/eval-helper/EvalHelper").getEvalMetrics;
+} catch {
+  // The components will remain null of it is not available
+}
 
 function PromptCard({
   promptDetails,
@@ -67,7 +87,6 @@ function PromptCard({
   const [result, setResult] = useState({
     promptOutputId: null,
     output: "",
-    evalMetrics: [],
   });
   const [outputIds, setOutputIds] = useState([]);
   const [coverage, setCoverage] = useState(0);
@@ -196,24 +215,6 @@ function PromptCard({
     setPage(newPage);
   };
 
-  const sortEvalMetricsByType = (metrics) => {
-    const sieve = {};
-    for (const metric of metrics) {
-      if (!sieve[metric.type]) {
-        sieve[metric.type] = [metric];
-      } else {
-        sieve[metric.type].push(metric);
-      }
-    }
-
-    let sortedMetrics = [];
-    for (const type of Object.keys(sieve)) {
-      sortedMetrics = sortedMetrics.concat(sieve[type]);
-    }
-
-    return sortedMetrics;
-  };
-
   const handleTypeChange = (value) => {
     handleChange(value, promptDetails?.prompt_id, "enforce_type", true).then(
       () => {
@@ -276,10 +277,11 @@ function PromptCard({
         }
 
         // Handle Eval
-        let evalMetrics = [];
-        if (promptDetails?.evaluate) {
-          evalMetrics = data[`${promptDetails?.prompt_key}__evaluation`] || [];
-        }
+        const evalMetrics = getEvalMetrics(
+          promptDetails?.evaluate,
+          promptDetails?.prompt_key,
+          data
+        );
         handleUpdateOutput(value, selectedDoc, evalMetrics, method, url);
       })
       .catch((err) => {
@@ -323,11 +325,11 @@ function PromptCard({
           }
 
           // Handle Eval
-          let evalMetrics = [];
-          if (promptDetails?.evaluate) {
-            evalMetrics =
-              data[`${promptDetails?.prompt_key}__evaluation`] || [];
-          }
+          const evalMetrics = getEvalMetrics(
+            promptDetails?.evaluate,
+            promptDetails?.prompt_key,
+            data
+          );
           handleUpdateOutput(outputValue, item, evalMetrics, method, url);
         })
         .catch((err) => {
@@ -429,7 +431,6 @@ function PromptCard({
       setResult({
         promptOutputId: null,
         output: "",
-        evalMetrics: [],
       });
       return;
     }
@@ -441,7 +442,6 @@ function PromptCard({
           setResult({
             promptOutputId: null,
             output: "",
-            evalMetrics: [],
           });
           return;
         }
@@ -726,21 +726,13 @@ function PromptCard({
           >
             <div className="prompt-card-llm-profiles">
               <Space direction="horizontal">
-                <div>
-                  <Button
-                    size="small"
-                    onClick={() => setOpenEval(true)}
-                    disabled={disableLlmOrDocChange.includes(
-                      promptDetails?.prompt_id
-                    )}
-                  >
-                    <Space>
-                      <Typography.Text className="font-size-12">
-                        Eval: {promptDetails?.evaluate ? "On" : "Off"}
-                      </Typography.Text>
-                    </Space>
-                  </Button>
-                </div>
+                {EvalBtn && (
+                  <EvalBtn
+                    btnText={promptDetails?.evaluate ? "On" : "Off"}
+                    promptId={promptDetails.prompt_id}
+                    setOpenEval={setOpenEval}
+                  />
+                )}
                 <Button
                   size="small"
                   type="link"
@@ -816,13 +808,7 @@ function PromptCard({
                 </Button>
               </div>
             </div>
-            {result?.evalMetrics?.length > 0 && (
-              <div>
-                {result?.evalMetrics.map((evalMetric) => (
-                  <EvalMetricTag key={evalMetric.id} metric={evalMetric} />
-                ))}
-              </div>
-            )}
+            {EvalMetrics && <EvalMetrics result={result} />}
           </Space>
         </>
         {(isRunLoading ||
@@ -841,12 +827,14 @@ function PromptCard({
           </>
         )}
       </Card>
-      <EvalModal
-        open={openEval}
-        setOpen={setOpenEval}
-        promptDetails={promptDetails}
-        handleChange={handleChange}
-      />
+      {EvalModal && (
+        <EvalModal
+          open={openEval}
+          setOpen={setOpenEval}
+          promptDetails={promptDetails}
+          handleChange={handleChange}
+        />
+      )}
       <OutputForDocModal
         open={openOutputForDoc}
         setOpen={setOpenOutputForDoc}
