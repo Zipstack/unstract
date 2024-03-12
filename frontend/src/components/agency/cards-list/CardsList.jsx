168f1fde96ac4f8dad0a277f008c121c014bcab0
vishnuszipstack <vishnu@zipstack.com>
2024-03-12 10:55:49 +0530

    code styling issue fix

--- frontend/src/components/agency/cards-list/CardsList.jsx
+++ frontend/src/components/agency/cards-list/CardsList.jsx
@@ -128,11 +128,7 @@ const CardsList = ({ step, index, activeTool, moveItem }) => {
                 </Typography.Text>
               </div>
               <div className="wf-step-card-progress display-flex-center">
-                <Progress
-                  type="circle"
-                  size="small"
-                  format={() => index + 1}
-                />
+                <Progress type="circle" size="small" format={() => index + 1} />
               </div>
             </div>
           </Col>
