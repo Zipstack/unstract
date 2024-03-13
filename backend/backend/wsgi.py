8ea8429e1727d2b0ba7e431f9bd7a596c8b0c73d
Hari John Kuriakose <hari@zipstack.com>
2024-03-13 15:54:30 +0530

    Move socket server to utils

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,18 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
-import socketio
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
-from log_events.views import sio
+
+from utils.websocket import start_server
 
 load_dotenv()
-path_prefix = settings.PATH_PREFIX 
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     os.environ.get("DJANGO_SETTINGS_MODULE", "backend.settings.dev"),
 )
+
 django_app = get_wsgi_application()
-application = socketio.WSGIApp(sio, django_app,socketio_path=f"{path_prefix}/socket")
+
+application = start_server(django_app, f"{settings.PATH_PREFIX}/socket")
