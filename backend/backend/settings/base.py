af0f350febbe64dcd5ca6134807cc809b1a6e757
ali-zipstack <muhammad.ali@zipstack.com>
2024-03-15 19:40:31 +0530

    reverted STATIC_URL to it's old state from taking varible

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -374,8 +374,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/api/v1/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = f"/{PATH_PREFIX}/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
