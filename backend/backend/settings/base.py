780ba9d04f2c8508f8cbbb47a9a24e75ad83f9fa
ali-zipstack <muhammad.ali@zipstack.com>
2024-03-15 17:58:16 +0530

    removed sessions from tenant applications list

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -193,7 +193,6 @@ TENANT_APPS = (
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "tenant_account",
