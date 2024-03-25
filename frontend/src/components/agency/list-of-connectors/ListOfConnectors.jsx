932ef84f7b79494943e43b99c2934d26e2bd2471
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2024-03-25 14:21:29 +0530

    disabled connectors and tagged coming soon

--- frontend/src/components/agency/list-of-connectors/ListOfConnectors.jsx
+++ frontend/src/components/agency/list-of-connectors/ListOfConnectors.jsx
@@ -1,5 +1,6 @@
-import { Menu } from "antd";
+import { Menu, Tooltip } from "antd";
 import PropTypes from "prop-types";
+import comingSoon from "../../../assets/coming-soon.png";
 
 function ListOfConnectors({ listOfConnectors, selectedId, handleSelectItem }) {
   return (
@@ -9,7 +10,40 @@ function ListOfConnectors({ listOfConnectors, selectedId, handleSelectItem }) {
         style={{ border: 0 }}
         selectedKeys={[selectedId]}
         mode="inline"
-        items={listOfConnectors}
+        items={listOfConnectors.map((item) => ({
+          label: (
+            <>
+              {item.isDisabled ? (
+                <Tooltip title="Coming Soon">
+                  <div
+                    style={{
+                      position: "relative",
+                      display: "inline-block",
+                      width: "100%",
+                    }}
+                  >
+                    <img
+                      src={comingSoon}
+                      alt="Coming Soon"
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        right: 0,
+                        width: "40px", // Adjust width and height according to your image size
+                        height: "40px",
+                      }}
+                    />
+                    {item.label}
+                  </div>
+                </Tooltip>
+              ) : (
+                item.label
+              )}
+            </>
+          ),
+          disabled: item.isDisabled,
+          icon: item.icon,
+        }))}
         onClick={handleSelectItem}
       />
     </div>
