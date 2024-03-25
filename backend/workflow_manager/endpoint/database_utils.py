d876f93e56eda471901df941ccf3c829f3d42f6f
kirtimanmishrazipstack <kirtiman.mishra@zipstack.com>
2024-03-25 10:15:06 +0530

    add debug log

--- backend/workflow_manager/endpoint/database_utils.py
+++ backend/workflow_manager/endpoint/database_utils.py
@@ -291,7 +291,7 @@ class DatabaseUtils:
             f"INSERT INTO {table_name} ({','.join(sql_keys)}) "
             f"SELECT {','.join(sql_values)}"
         )
-        logger.info(f"insertng into table with: {sql} query")
+        logger.debug(f"insertng into table with: {sql} query")
         try:
             if hasattr(engine, "cursor"):
                 with engine.cursor() as cursor:
@@ -341,7 +341,7 @@ class DatabaseUtils:
         sql = DBConnectorQueryHelper.create_table_query(
             conn_cls=class_name, table=table_name, database_entry=database_entry
         )
-        logger.info(f"creating table with: {sql} query")
+        logger.debug(f"creating table with: {sql} query")
         try:
             if hasattr(engine, "cursor"):
                 with engine.cursor() as cursor:
