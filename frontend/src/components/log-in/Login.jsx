2a8aa6d5c33b69d3d3591092b75da7e8e34cfe52
Athul <89829560+athul-rs@users.noreply.github.com>
2024-04-04 13:51:37 +0530

    fix/login-button-issue (#230)
    * fix/login-button-issue
    
    * unused button  function
    
    * updated design for login
    
    ---------
    
    Co-authored-by: Neha <115609453+nehabagdia@users.noreply.github.com>

--- frontend/src/components/log-in/Login.jsx
+++ frontend/src/components/log-in/Login.jsx
@@ -1,7 +1,8 @@
 import { Typography, Button } from "antd";
+import { Row, Col } from "antd";
 
 import logo from "../../assets/UnstractLogoBlack.svg";
-import loginRightBanner from "../../assets/login_page_right_banner.svg";
+import loginRightBanner from "../../assets/login-right-panel.svg";
 import { getBaseUrl } from "../../helpers/GetStaticData";
 import "./Login.css";
 
@@ -17,57 +18,50 @@ function Login() {
   const handleLogin = () => {
     window.location.href = newURL;
   };
-  const handleSignin = () => {
-    window.location.href = newURL;
-  };
 
   return (
     <div className="login-main">
-      <div className="login-left-section">
-        <div className="button-wraper">
-          <img src={logo} alt="Logo" className="logo" />
-          {LoginForm && (
-            <LoginForm handleLogin={handleLogin} handleSignin={handleSignin} />
-          )}
-          {!LoginForm && (
-            <div>
-              <Button
-                className="login-button button-margin"
-                onClick={handleLogin}
-              >
-                Login
-              </Button>
-              <Button
-                className="signin-button button-margin"
-                onClick={handleSignin}
-              >
-                Sign in
-              </Button>
+      <Row>
+        <Col xs={24} md={12} className="login-left-section">
+          <div className="button-wraper">
+            <img src={logo} alt="Logo" className="logo" />
+            {LoginForm && <LoginForm handleLogin={handleLogin} />}
+            {!LoginForm && (
+              <div>
+                <Button
+                  className="login-button button-margin"
+                  onClick={handleLogin}
+                >
+                  Login
+                </Button>
+              </div>
+            )}
+          </div>
+        </Col>
+        <Col xs={24} md={12} className="login-right-section">
+          <div className="right-section-text-wrapper">
+            <div className="right-title-cover">
+              <Typography.Title className="right-section-title">
+                UNLOCK VALUE FROM UNSTRUCTURED DATA.
+              </Typography.Title>
             </div>
-          )}
-        </div>
-      </div>
-      <div className="login-right-section">
-        <div className="right-section-text-wrapper">
-          <div className="right-title-cover">
-            <Typography.Title className="right-section-title">
-              UNLOCK VALUE FROM UNSTRUCTURED DATA.
-            </Typography.Title>
+            <div className="right-subtitle-cover">
+              <Typography align="center" className="right-subtitle">
+                Unstract is a no-code LLM platform that lets you automate even
+                the most complex workflows involving unstructured data, saving
+                you time, money, and automation headaches.
+              </Typography>
+            </div>
+            <div>
+              <img
+                src={loginRightBanner}
+                alt="login background"
+                className="login-background"
+              />
+            </div>
           </div>
-          <div className="right-subtitle-cover">
-            <Typography align="center" className="right-subtitle">
-              Unstract is a no-code LLM platform that lets you automate even the
-              most complex workflows involving unstructured data, saving you
-              time, money, and automation headaches.
-            </Typography>
-          </div>
-          <img
-            src={loginRightBanner}
-            alt="login background"
-            className="login-background"
-          />
-        </div>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 }
