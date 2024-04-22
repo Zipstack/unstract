c151be8f32ee3e711a1dece5afa7de4b2f787eac
Deepak <deepak@zipstack.com>
2024-04-22 18:27:37 +0530

    Made token_usage table tenant specific

---
+++ backend/usage/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Usage
+
+admin.site.register(Usage)
---
+++ backend/usage/models.py
@@ -0,0 +1,25 @@
+import uuid
+
+from django.db import models
+from utils.models.base_model import BaseModel
+
+
+class Usage(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    organization_id = models.CharField(max_length=255)
+    workflow_id = models.CharField(max_length=255)
+    execution_id = models.CharField(max_length=255)
+    adapter_instance_id = models.CharField(max_length=255)
+    run_id = models.CharField(max_length=255)
+    usage_type = models.CharField(max_length=255)
+    model_type = models.CharField(max_length=255)
+    embedding_tokens = models.IntegerField()
+    prompt_tokens = models.IntegerField()
+    completion_tokens = models.IntegerField()
+    total_tokens = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = "token_usage"
