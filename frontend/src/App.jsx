d22fa5e8af304537a4838168bf7984de021ccc2a
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2024-04-24 17:10:30 +0530

    Added destroy all button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { ConfigProvider, notification, theme } from "antd";
+import { Button, ConfigProvider, Space, notification, theme } from "antd";
 import { BrowserRouter } from "react-router-dom";
 
 import { THEME } from "./helpers/GetStaticData.js";
@@ -13,12 +13,25 @@ function App() {
   const { sessionDetails } = useSessionStore();
   const { alertDetails } = useAlertStore();
 
+  const btn = (
+    <Space>
+      <Button
+        type="link"
+        size="small"
+        onClick={() => notificationAPI.destroy()}
+      >
+        Destroy All
+      </Button>
+    </Space>
+  );
+
   alertDetails.content &&
     notificationAPI.open({
       message: alertDetails.title,
       description: alertDetails.content,
       type: alertDetails.type,
       duration: alertDetails.duration,
+      btn,
     });
 
   return (
