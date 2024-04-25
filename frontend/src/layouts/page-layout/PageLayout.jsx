0d7f44af03497191b1f525bd197a7a965e8dcb97
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2024-04-25 15:22:14 +0530

    added logs component to layout

--- frontend/src/layouts/page-layout/PageLayout.jsx
+++ frontend/src/layouts/page-layout/PageLayout.jsx
@@ -1,13 +1,80 @@
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
-import { Button, Layout } from "antd";
+import {
+  FullscreenExitOutlined,
+  FullscreenOutlined,
+  LeftOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import { Button, Collapse, Layout, Modal } from "antd";
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./PageLayout.css";
 
 import SideNavBar from "../../components/navigations/side-nav-bar/SideNavBar.jsx";
 import { TopNavBar } from "../../components/navigations/top-nav-bar/TopNavBar.jsx";
+import { Footer } from "antd/es/layout/layout.js";
+import { DisplayLogs } from "../../components/custom-tools/display-logs/DisplayLogs.jsx";
+import { LogsLabel } from "../../components/agency/logs-label/LogsLabel.jsx";
+import { useResize } from "../../hooks/useResize.jsx";
 
 function PageLayout() {
+  const [showLogsModal, setShowLogsModal] = useState(false);
+  const [activeKey, setActiveKey] = useState([]);
+  const { height, enableResize, setHeight } = useResize({ minHeight: 50 });
+
+  const openLogsModal = () => {
+    setShowLogsModal(true);
+  };
+
+  const closeLogsModal = () => {
+    setShowLogsModal(false);
+  };
+  const handleCollapse = (keys) => {
+    setActiveKey(keys);
+    console.log(collapsed);
+    setHeight(keys.length > 0 ? 200 : 50);
+  };
+
+  const getItems = () => [
+    {
+      key: "1",
+      label:
+        activeKey?.length > 0 ? (
+          <>
+            <div
+              style={{
+                position: "absolute",
+                width: "100%",
+                top: 0,
+                cursor: "row-resize",
+                height: "10px",
+              }}
+              onMouseDown={enableResize}
+              onClick={(e) => e.stopPropagation()}
+            />
+            <LogsLabel />
+          </>
+        ) : (
+          "Logs"
+        ),
+      children: (
+        <div className="agency-ide-logs">
+          <DisplayLogs />
+        </div>
+      ),
+      extra: genExtra(),
+    },
+  ];
+
+  const genExtra = () => (
+    <FullscreenOutlined
+      onClick={(event) => {
+        // If you don't want click extra trigger collapse, you can prevent this:
+        openLogsModal();
+        event.stopPropagation();
+      }}
+    />
+  );
+
   const initialCollapsedValue =
     JSON.parse(localStorage.getItem("collapsed")) || false;
   const [collapsed, setCollapsed] = useState(initialCollapsedValue);
@@ -29,6 +96,32 @@ function PageLayout() {
             className="collapse_btn"
           />
           <Outlet />
+          <Footer className="log-footer">
+            <Collapse
+              className="ide-collapse-panel"
+              size="small"
+              activeKey={activeKey}
+              items={getItems()}
+              expandIconPosition="end"
+              onChange={handleCollapse}
+              bordered={false}
+              style={{ height: height }}
+            />
+            <Modal
+              title="Logs"
+              open={showLogsModal}
+              onCancel={closeLogsModal}
+              className="agency-ide-log-modal"
+              footer={null}
+              width={1000}
+              closeIcon={<FullscreenExitOutlined />}
+            >
+              <LogsLabel />
+              <div className="agency-ide-logs">
+                <DisplayLogs />
+              </div>
+            </Modal>
+          </Footer>
         </Layout>
       </Layout>
     </div>
