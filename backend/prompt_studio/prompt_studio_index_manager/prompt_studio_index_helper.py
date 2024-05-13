1fb2a6cc86827574235c26515bcec150313d8033
gayathrivijayakumar <gayathri@zipstack.com>
2024-05-13 14:47:06 +0530

    Handling multi-prompt run

--- backend/prompt_studio/prompt_studio_index_manager/prompt_studio_index_helper.py
+++ backend/prompt_studio/prompt_studio_index_manager/prompt_studio_index_helper.py
@@ -1,10 +1,10 @@
 import json
 import logging
-from typing import Optional
 
 from prompt_studio.prompt_profile_manager.models import ProfileManager
 from prompt_studio.prompt_studio_document_manager.models import DocumentManager
 
+from ..prompt_studio_core.exceptions import IndexingAPIError
 from .models import IndexManager
 
 logger = logging.getLogger(__name__)
@@ -24,32 +24,29 @@ class PromptStudioIndexHelper:
         if is_summary:
             index_id = "summarize_index_id"
 
+        args: dict[str, str] = dict()
+        args["document_manager"] = document
+        args["profile_manager"] = profile_manager
+        args[index_id] = doc_id
+
         try:
-            index_manager: Optional[IndexManager] = IndexManager.objects.get(
-                document_manager=document, profile_manager=profile_manager
-            )
-        except IndexManager.DoesNotExist:
-            index_manager = None
+            # Create or get the existing record for this document and
+            # profile combo
+            index_manager, success = IndexManager.objects.get_or_create(**args)
 
-        args: dict[str, str] = {f"{index_id}": doc_id}
-
-        index_ids_list = []
-        if index_manager:
             index_ids = index_manager.index_ids_history
             index_ids_list = json.loads(index_ids) if index_ids else []
             if doc_id not in index_ids:
                 index_ids_list.append(doc_id)
-        else:
-            index_ids_list.append(doc_id)
 
-        args["index_ids_history"] = json.dumps(index_ids_list)
+            args["index_ids_history"] = json.dumps(index_ids_list)
 
-        if index_manager:
+            # Update the record with the index id
             result: IndexManager = IndexManager.objects.filter(
                 index_manager_id=index_manager.index_manager_id
             ).update(**args)
-        else:
-            args["document_manager"] = document
-            args["profile_manager"] = profile_manager
-            result = IndexManager.objects.create(**args)
+
+        except Exception as e:
+            raise IndexingAPIError("Error updating indexing status") from e
+
         return result
