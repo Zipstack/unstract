0755276d4302f038fb59a56309e412a26b6a71a0
Chandrasekharan M <chandrasekharan@zipstack.com>
2024-06-21 21:07:47 +0530

    Docs on editing runtime config for services, structure tool env comments

--- run-platform.sh
+++ run-platform.sh
@@ -8,6 +8,7 @@ blue_text='\033[94m'
 green_text='\033[32m'
 red_text='\033[31m'
 default_text='\033[39m'
+yellow_text='\033[33m'
 
 # set -x/xtrace uses PS4 for more info
 PS4="$blue_text""${0}:${LINENO}: ""$default_text"
@@ -250,6 +251,9 @@ run_services() {
   echo -e "\nOnce the services are up, visit ""$blue_text""http://frontend.unstract.localhost""$default_text"" in your browser."
   echo "See logs with:"
   echo -e "    ""$blue_text""$docker_compose_cmd -f docker/docker-compose.yaml logs -f""$default_text"
+  echo -e "Configure services by updating their ""$yellow_text"".env""$default_text"" files."
+  echo "Make sure to restart the services with:"
+  echo -e "    ""$blue_text""$docker_compose_cmd -f docker/docker-compose.yaml up -d""$default_text"
 
   popd 1>/dev/null
 }
@@ -274,9 +278,9 @@ services=($(VERSION=$opt_version $docker_compose_cmd -f $script_dir/docker/docke
 display_banner
 parse_args $*
 
-do_git_pull
-setup_env
-build_services
+# do_git_pull
+# setup_env
+# build_services
 run_services
 #
 # Run Unstract platform - END
