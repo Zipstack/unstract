46f7629f5f570820d8e96e1fd4c8ad1ea00e6ea4
johnyrahul <rahul.johny@zipstack.com>
2024-06-24 14:10:25 +0530

    Add info text for highlight option

--- frontend/src/components/custom-tools/highlight-metadata/HighLightMetaData.jsx
+++ frontend/src/components/custom-tools/highlight-metadata/HighLightMetaData.jsx
@@ -1,4 +1,4 @@
-import { Checkbox, Form, Space, Typography } from "antd";
+import { Checkbox, Form, Space, Typography, Alert } from "antd";
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 
@@ -7,6 +7,7 @@ import { CustomButton } from "../../../components/widgets/custom-button/CustomBu
 import { useCustomToolStore } from "../../../store/custom-tool-store";
 import { useAlertStore } from "../../../store/alert-store";
 import { useExceptionHandler } from "../../../hooks/useExceptionHandler";
+import "./HighLightMetaData.css";
 
 function HighLightMetaData({ handleUpdateTool }) {
   const [enableHighlighting, setEnableHighlighting] = useState(false);
@@ -63,6 +64,29 @@ function HighLightMetaData({ handleUpdateTool }) {
               onChange={(e) => setEnableHighlighting(e.target.checked)}
             />
             <Typography.Text>Enable Highlighting</Typography.Text>
+          </Space>
+          <Space>
+            <Alert
+              className="highlight"
+              message={
+                <>
+                  Turn this option on if downstream use cases need you to
+                  highlight extracted portions in the source document. e.g: for
+                  manual review. This option instructs LLMWhisperer to store
+                  highlighting information, which can be extracted using the
+                  returned Whisper Hash. Please see the{" "}
+                  <a
+                    href="https://docs.unstract.com/llm_whisperer/apis/llm_whisperer_text_extraction_highlight_api"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Highlighting API documentation
+                  </a>{" "}
+                  for more details.
+                </>
+              }
+              type="info"
+            />
           </Space>
         </Form.Item>
         <Form.Item className="display-flex-right">
