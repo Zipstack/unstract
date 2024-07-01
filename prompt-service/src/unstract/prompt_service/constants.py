9c04811039068b47034cf315c337da11c38d191f
ali <117142933+muhammad-ali-e@users.noreply.github.com>
2024-07-01 09:34:57 +0530

    unstract-multi-tenantV2 feature-flag changes (#422)
    * unstract-multi-tenantV2 feature-flag changes
    
    * featureflag function bring to under flags package to avoid dplication
    
    * resolved merge conflicts with main
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * precommit lints
    
    * remove feature flags utiliy function from backend
    
    * cleanup prints statement from unstract flipt packages and added warning instead
    
    * sonar issue resolved from logger
    
    ---------
    
    Signed-off-by: ali <117142933+muhammad-ali-e@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- prompt-service/src/unstract/prompt_service/constants.py
+++ prompt-service/src/unstract/prompt_service/constants.py
@@ -75,3 +75,19 @@ class RunLevel(Enum):
     RUN = "RUN"
     EVAL = "EVAL"
     CHALLENGE = "CHALLENGE"
+
+
+class FeatureFlag:
+    """Temporary feature flags."""
+
+    MULTI_TENANCY_V2 = "multi_tenancy_v2"
+
+
+class DBTableV2:
+    """Database tables."""
+
+    ORGANIZATION = "organization_v2"
+    ADAPTER_INSTANCE = "adapter_instance_v2"
+    PROMPT_STUDIO_REGISTRY = "prompt_studio_registry_v2"
+    PLATFORM_KEY = "platform_key_v2"
+    TOKEN_USAGE = "token_usage_v2"
