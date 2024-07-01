c60a63cfff669470df11240525cde1d3289a26f0
ali <117142933+muhammad-ali-e@users.noreply.github.com>
2024-07-01 11:57:43 +0530

    Feat/multi tenancy v2 context and organization (#430)
    * unstract-multi-tenantV2 feature-flag changes
    
    * featureflag function bring to under flags package to avoid dplication
    
    * resolved merge conflicts with main
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * precommit lints
    
    * remove feature flags utiliy function from backend
    
    * cleanup prints statement from unstract flipt packages and added warning instead
    
    * sonar issue resolved from logger
    
    * User context and Organization mixins to use for v2
    
    ---------
    
    Signed-off-by: ali <117142933+muhammad-ali-e@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/utils/user_session.py
+++ backend/utils/user_session.py
@@ -1,12 +1,20 @@
 from typing import Optional
 
 from django.http import HttpRequest
+from utils.constants import FeatureFlag
+
+from unstract.flags.feature_flag import check_feature_flag_status
 
 
 class UserSessionUtils:
     @staticmethod
     def get_organization_id(request: HttpRequest) -> Optional[str]:
-        return request.session.get("organization")
+        session_org_id = request.session.get("organization")
+        if check_feature_flag_status(FeatureFlag.MULTI_TENANCY_V2):
+            requested_org_id = request.organization_id
+            if requested_org_id and (session_org_id != requested_org_id):
+                return None
+        return session_org_id
 
     @staticmethod
     def set_organization_id(request: HttpRequest, organization_id: str) -> None:
