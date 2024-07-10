a6e1d21f0fdc7eb45d72f0e4633fb4352f05439a
ali-zipstack <muhammad.ali@zipstack.com>
2024-07-10 13:05:00 +0530

    v2 changes of pipeline and api

---
+++ backend/api_v2/key_helper.py
@@ -0,0 +1,72 @@
+import logging
+
+from api_v2.exceptions import Forbidden, UnauthorizedKey
+from api_v2.models import APIDeployment, APIKey
+from api_v2.serializers import APIKeySerializer
+from workflow_manager.workflow_v2.workflow_helper import WorkflowHelper
+
+logger = logging.getLogger(__name__)
+
+
+class KeyHelper:
+    @staticmethod
+    def validate_api_key(api_key: str, api_instance: APIDeployment) -> None:
+        """Validate api key.
+
+        Args:
+            api_key (str): api key from request
+            api_instance (APIDeployment): api deployment instance
+
+        Raises:
+            Forbidden: _description_
+        """
+        try:
+            api_key_instance: APIKey = APIKey.objects.get(api_key=api_key)
+            if not KeyHelper.has_access(api_key_instance, api_instance):
+                raise UnauthorizedKey()
+        except APIKey.DoesNotExist:
+            raise UnauthorizedKey()
+        except APIDeployment.DoesNotExist:
+            raise Forbidden("API not found.")
+
+    @staticmethod
+    def list_api_keys_of_api(api_instance: APIDeployment) -> list[APIKey]:
+        api_keys: list[APIKey] = APIKey.objects.filter(api=api_instance).all()
+        return api_keys
+
+    @staticmethod
+    def has_access(api_key: APIKey, api_instance: APIDeployment) -> bool:
+        """Check if the provided API key has access to the specified API
+        instance.
+
+        Args:
+            api_key (APIKey): api key associated with the api
+            api_instance (APIDeployment): api model
+
+        Returns:
+            bool: True if allowed to execute, False otherwise
+        """
+        if not api_key.is_active:
+            return False
+        if isinstance(api_key.api, APIDeployment):
+            return api_key.api == api_instance
+        return False
+
+    @staticmethod
+    def validate_workflow_exists(workflow_id: str) -> None:
+        """Validate that the specified workflow_id exists in the Workflow
+        model."""
+        WorkflowHelper.get_workflow_by_id(workflow_id)
+
+    @staticmethod
+    def create_api_key(deployment: APIDeployment) -> APIKey:
+        """Create an APIKey entity with the data from the provided
+        APIDeployment instance."""
+        # Create an instance of the APIKey model
+        api_key_serializer = APIKeySerializer(
+            data={"api": deployment.id, "description": "Initial Access Key"},
+            context={"deployment": deployment},
+        )
+        api_key_serializer.is_valid(raise_exception=True)
+        api_key: APIKey = api_key_serializer.save()
+        return api_key
---
+++ backend/pipeline_v2/serializers/crud.py
@@ -0,0 +1,104 @@
+import logging
+from collections import OrderedDict
+from typing import Any
+
+from connector_processor.connector_processor import ConnectorProcessor
+from connector_v2.connector_instance_helper import ConnectorInstanceHelper
+from connector_v2.models import ConnectorInstance
+from pipeline_v2.constants import PipelineConstants as PC
+from pipeline_v2.constants import PipelineKey as PK
+from pipeline_v2.models import Pipeline
+from scheduler.helper import SchedulerHelper
+from utils.serializer_utils import SerializerUtils
+
+from backend.serializers import AuditSerializer
+from unstract.connectors.connectorkit import Connectorkit
+
+logger = logging.getLogger(__name__)
+
+
+class PipelineSerializer(AuditSerializer):
+
+    class Meta:
+        model = Pipeline
+        fields = "__all__"
+
+    def create(self, validated_data: dict[str, Any]) -> Any:
+        # TODO: Deduce pipeline type based on WF?
+        validated_data[PK.ACTIVE] = True  # Add this as default instead?
+        validated_data[PK.SCHEDULED] = True
+        return super().create(validated_data)
+
+    def save(self, **kwargs: Any) -> Pipeline:
+        pipeline: Pipeline = super().save(**kwargs)
+        SchedulerHelper.add_job(
+            str(pipeline.pk),
+            cron_string=pipeline.cron_string,
+        )
+        return pipeline
+
+    def _get_name_and_icon(self, connectors: list[Any], connector_id: Any) -> Any:
+        for obj in connectors:
+            if obj["id"] == connector_id:
+                return obj["name"], obj["icon"]
+        return PC.NOT_CONFIGURED, None
+
+    def _add_connector_data(
+        self,
+        repr: OrderedDict[str, Any],
+        connector_instance_list: list[Any],
+        connectors: list[Any],
+    ) -> OrderedDict[str, Any]:
+        """Adds connector Input/Output data.
+
+        Args:
+            sef (_type_): _description_
+            repr (OrderedDict[str, Any]): _description_
+
+        Returns:
+            OrderedDict[str, Any]: _description_
+        """
+        repr[PC.SOURCE_NAME] = PC.NOT_CONFIGURED
+        repr[PC.DESTINATION_NAME] = PC.NOT_CONFIGURED
+        for instance in connector_instance_list:
+            if instance.connector_type == "INPUT":
+                repr[PC.SOURCE_NAME], repr[PC.SOURCE_ICON] = self._get_name_and_icon(
+                    connectors=connectors,
+                    connector_id=instance.connector_id,
+                )
+            if instance.connector_type == "OUTPUT":
+                repr[PC.DESTINATION_NAME], repr[PC.DESTINATION_ICON] = (
+                    self._get_name_and_icon(
+                        connectors=connectors,
+                        connector_id=instance.connector_id,
+                    )
+                )
+        return repr
+
+    def to_representation(self, instance: Pipeline) -> OrderedDict[str, Any]:
+        """To set Source, Destination & Agency for Pipelines."""
+        repr: OrderedDict[str, Any] = super().to_representation(instance)
+
+        connector_kit = Connectorkit()
+        connectors = connector_kit.get_connectors_list()
+
+        if SerializerUtils.check_context_for_GET_or_POST(context=self.context):
+            workflow = instance.workflow
+            connector_instance_list = ConnectorInstanceHelper.get_input_output_connector_instances_by_workflow(  # noqa
+                workflow.id
+            )
+            repr[PK.WORKFLOW_ID] = workflow.id
+            repr[PK.WORKFLOW_NAME] = workflow.workflow_name
+            repr[PK.CRON_STRING] = repr.pop(PK.CRON_STRING)
+            repr = self._add_connector_data(
+                repr=repr,
+                connector_instance_list=connector_instance_list,
+                connectors=connectors,
+            )
+
+        return repr
+
+    def get_connector_data(self, connector: ConnectorInstance, key: str) -> Any:
+        return ConnectorProcessor.get_connector_data_with_key(
+            connector.connector_id, key
+        )
