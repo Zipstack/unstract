8f0a5981e28b443f46ed6adb5490322d96ac7720
ali-zipstack <muhammad.ali@zipstack.com>
2024-07-10 17:31:33 +0530

    v2 changes of ProfileManager,core and studio

---
+++ backend/prompt_studio/prompt_studio_core_v2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CustomTool(AppConfig):
+    name = "prompt_studio.prompt_studio_core_v2"
---
+++ backend/prompt_studio/prompt_studio_v2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ToolStudioPrompt(AppConfig):
+    name = "prompt_studio.prompt_studio_v2"
