80fa06682e07ff53deb0bf13abb89c762b43d220
ali-zipstack <muhammad.ali@zipstack.com>
2024-07-16 16:26:11 +0530

    Added the TODO comment for urls

--- backend/usage_v2/urls.py
+++ backend/usage_v2/urls.py
@@ -5,6 +5,8 @@ from .views import UsageView
 
 get_token_usage = UsageView.as_view({"get": "get_token_usage"})
 
+# TODO: Refactor URL to avoid using action-specific verbs like get.
+
 urlpatterns = format_suffix_patterns(
     [
         path(
