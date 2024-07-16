2484a006a02fc78fde035aaf9338636cb3e94111
Rahul Johny <116638720+johnyrahul@users.noreply.github.com>
2024-07-16 16:17:36 +0530

    Update docker/scripts/merge_env.py
    Co-authored-by: ali <117142933+muhammad-ali-e@users.noreply.github.com>
    Signed-off-by: Rahul Johny <116638720+johnyrahul@users.noreply.github.com>

--- docker/scripts/merge_env.py
+++ docker/scripts/merge_env.py
@@ -54,7 +54,7 @@ def _merge_to_env_file(base_env_file_path: str, target_env: dict[str, str] = {})
     target env.
 
     Args:
-        base_env_path (string): Base env file path e.g. `sample.env`
+        base_env_file_path (string): Base env file path e.g. `sample.env`
         target_env (dict, optional): Target env to use for merge e.g. `.env`
 
     Returns:
