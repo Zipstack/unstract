9664b62eeb4e2829bd8af30e6593708691f619da
ali-zipstack <muhammad.ali@zipstack.com>
2024-07-16 11:31:10 +0530

    addressed comments

--- backend/prompt_studio/prompt_studio_registry_v2/fields.py
+++ backend/prompt_studio/prompt_studio_registry_v2/fields.py
@@ -5,25 +5,27 @@ from django.db import models
 logger = logging.getLogger(__name__)
 
 
-class ToolPropertyJSONField(models.JSONField):
+class ToolBaseJSONField(models.JSONField):
     def from_db_value(self, value, expression, connection):  # type: ignore
         metadata = super().from_db_value(value, expression, connection)
         return metadata
 
 
-class ToolSpecJSONField(models.JSONField):
-    def from_db_value(self, value, expression, connection):  # type: ignore
-        metadata = super().from_db_value(value, expression, connection)
-        return metadata
+# TODO: Investigate if ToolBaseJSONField can replace the need for ToolPropertyJSONField,
+#       ToolSpecJSONField, ToolVariablesJSONField, and ToolMetadataJSONField classes.
 
 
-class ToolVariablesJSONField(models.JSONField):
-    def from_db_value(self, value, expression, connection):  # type: ignore
-        metadata = super().from_db_value(value, expression, connection)
-        return metadata
+class ToolPropertyJSONField(ToolBaseJSONField, models.JSONField):
+    pass
 
 
-class ToolMetadataJSONField(models.JSONField):
-    def from_db_value(self, value, expression, connection):  # type: ignore
-        metadata = super().from_db_value(value, expression, connection)
-        return metadata
+class ToolSpecJSONField(ToolBaseJSONField, models.JSONField):
+    pass
+
+
+class ToolVariablesJSONField(ToolBaseJSONField, models.JSONField):
+    pass
+
+
+class ToolMetadataJSONField(ToolBaseJSONField, models.JSONField):
+    pass
