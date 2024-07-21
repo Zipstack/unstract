f5a33e997502c006d5e908659f910e7d01185e74
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2024-07-21 19:19:06 +0530

    minor fixes

--- frontend/src/components/custom-tools/prompt-card/PromptCardItems.jsx
+++ frontend/src/components/custom-tools/prompt-card/PromptCardItems.jsx
@@ -114,7 +114,7 @@ function PromptCardItems({
         result.conf[key.label] =
           adapter?.model || adapter?.adapter_id?.split("|")[0];
         if (adapter?.adapter_type === "LLM") result.icon = adapter?.icon;
-        result.conf["Profile Name"] = profile.profile_name;
+        result.conf["Profile Name"] = profile?.profile_name;
       }
     });
     return result;
