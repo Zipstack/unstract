ce603d884b30c85d4809c7ddb37b3fe4c2a47c3c
Chandrasekharan M <chandrasekharan@zipstack.com>
2024-08-14 15:47:32 +0530

    Refactored platform service to use blueprints and breakdown large main file

---
+++ platform-service/src/unstract/platform_service/run.py
@@ -0,0 +1,40 @@
+from logging.config import dictConfig
+
+from dotenv import load_dotenv
+from flask import Flask
+from unstract.platform_service.controller import api
+
+load_dotenv()
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            }
+        },
+        "root": {"level": "INFO", "handlers": ["wsgi"]},
+    }
+)
+
+
+def create_app() -> Flask:
+    app = Flask("platform_service")
+
+    app.register_blueprint(api)
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    # Start the server
+    app.run(host="0.0.0.0", port=3001, load_dotenv=True)
