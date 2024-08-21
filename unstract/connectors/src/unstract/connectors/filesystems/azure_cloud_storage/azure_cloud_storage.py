c8f7770d0e2dfe7f5e479b14a343b5a40d3e2db7
Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
2024-08-21 05:41:47 +0530

    fix: Removed unused bucket field from connectors (#596)
    Removed unused bucket field from connectors - no migrations done to remove existing records
    
    Co-authored-by: Ritwik G <100672805+ritwik-g@users.noreply.github.com>
    Co-authored-by: Neha <115609453+nehabagdia@users.noreply.github.com>

--- unstract/connectors/src/unstract/connectors/filesystems/azure_cloud_storage/azure_cloud_storage.py
+++ unstract/connectors/src/unstract/connectors/filesystems/azure_cloud_storage/azure_cloud_storage.py
@@ -15,7 +15,6 @@ class AzureCloudStorageFS(UnstractFileSystem):
         super().__init__("AzureCloudStorageFS")
         account_name = settings.get("account_name", "")
         access_key = settings.get("access_key", "")
-        self.bucket = settings.get("bucket", "")
         self.azure_fs = AzureBlobFileSystem(
             account_name=account_name, credential=access_key
         )
@@ -65,9 +64,9 @@ class AzureCloudStorageFS(UnstractFileSystem):
     def test_credentials(self) -> bool:
         """To test credentials for Azure Cloud Storage."""
         try:
-            is_dir = bool(self.get_fsspec_fs().isdir(self.bucket))
+            is_dir = bool(self.get_fsspec_fs().isdir(""))
             if not is_dir:
-                raise RuntimeError(f"'{self.bucket}' is not a valid bucket.")
+                raise RuntimeError("Could not access root directory.")
         except Exception as e:
             raise ConnectorError(
                 f"Error from Azure Cloud Storage while testing connection: {str(e)}"
