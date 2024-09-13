5a594da5ccb57440ed179d92dc82820c2f8dd37c
harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
2024-09-13 11:12:05 +0530

    [FIX] Re connection for close db connection & Removing connection Pool  (#685)
    Introducing reconnection for closed service
    
    Co-authored-by: Rahul Johny <116638720+johnyrahul@users.noreply.github.com>

--- platform-service/src/unstract/platform_service/helper/prompt_studio.py
+++ platform-service/src/unstract/platform_service/helper/prompt_studio.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from playhouse.pool import PooledPostgresqlDatabase
+from peewee import PostgresqlDatabase
 from unstract.platform_service.constants import DBTableV2, FeatureFlag
 from unstract.platform_service.exceptions import APIError
 
@@ -10,14 +10,14 @@ from unstract.flags.feature_flag import check_feature_flag_status
 class PromptStudioRequestHelper:
     @staticmethod
     def get_prompt_instance_from_db(
-        db_instance: PooledPostgresqlDatabase,
+        db_instance: PostgresqlDatabase,
         organization_id: str,
         prompt_registry_id: str,
     ) -> dict[str, Any]:
         """Get prompt studio registry from Backend Database.
 
         Args:
-            db_instance (PooledPostgresqlDatabase): Backend DB Connection Pool
+            db_instance (PostgresqlDatabase): Backend DB Connection
             organization_id (str): organization schema id
             prompt_registry_id (str): prompt_registry_id
 
@@ -45,5 +45,4 @@ class PromptStudioRequestHelper:
         columns = [desc[0] for desc in cursor.description]
         data_dict: dict[str, Any] = dict(zip(columns, result_row))
         cursor.close()
-        db_instance.close()
         return data_dict
