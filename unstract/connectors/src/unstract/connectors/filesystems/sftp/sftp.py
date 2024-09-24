57a89e9839e9ec9a137346b3ba8c88ead193b097
Kirtiman Mishra <110175055+kirtimanmishrazipstack@users.noreply.github.com>
2024-09-24 13:01:27 +0530

    feat/sftp-updated-changes (#710)
    * adding docker compose for sftp
    
    * adding sample env
    
    * changes in sftp
    
    * changes
    
    * changes
    
    * adding user id in docker compose
    
    * error handling for sftp
    
    * PR comments
    
    * PR comments

--- unstract/connectors/src/unstract/connectors/filesystems/sftp/sftp.py
+++ unstract/connectors/src/unstract/connectors/filesystems/sftp/sftp.py
@@ -1,13 +1,11 @@
 import logging
+import os
 from typing import Any
 
 from fsspec.implementations.sftp import SFTPFileSystem
 
-from unstract.connectors.exceptions import ConnectorError
+from unstract.connectors.exceptions import ConnectorError, PermissionDeniedError
 from unstract.connectors.filesystems.unstract_file_system import UnstractFileSystem
-
-# from sshfs import SSHFileSystem
-
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +15,7 @@ class SettingsKey:
     PORT = "port"
     USERNAME = "username"
     PASSWORD = "password"
+    USER_DIRECTORY = "user_dir"
 
 
 class SettingsDefault:
@@ -30,12 +29,14 @@ class SftpFS(UnstractFileSystem):
         port = int(settings.get(SettingsKey.PORT, SettingsDefault.PORT))
         username = settings.get(SettingsKey.USERNAME)
         password = settings.get(SettingsKey.PASSWORD)
+        self.directory = str(settings.get(SettingsKey.USER_DIRECTORY))
 
         self.sftp_fs = SFTPFileSystem(
-            host=host, port=port, username=username, password=password
+            host=host,
+            port=port,
+            username=username,
+            password=password,
         )
-        # TODO: Use sshfs if above implementation doesn't work as expected
-        # self.sftp_fs = SSHFileSystem(host=host, port=port, username=username, password=password)  # noqa: E501
 
     @staticmethod
     def get_id() -> str:
@@ -51,7 +52,7 @@ class SftpFS(UnstractFileSystem):
 
     @staticmethod
     def get_icon() -> str:
-        return "/icons/connector-icons/SFTP.svg"
+        return "/icons/connector-icons/SFTP.png"
 
     @staticmethod
     def can_write() -> bool:
@@ -64,11 +65,33 @@ class SftpFS(UnstractFileSystem):
     def get_fsspec_fs(self) -> SFTPFileSystem:
         return self.sftp_fs
 
+    # TODO: Check if this method can be removed, and use it from parent class
+    # (class UnstractFileSystem)
+    @staticmethod
+    def get_json_schema() -> str:
+        f = open(f"{os.path.dirname(__file__)}/static/json_schema.json")
+        schema = f.read()
+        f.close()
+        return schema
+
+    # TODO: Check if this method can be removed, and use it from parent class
+    # (class UnstractFileSystem)
+    @staticmethod
+    def requires_oauth() -> bool:
+        return False
+
+    # TODO: Check if this method can be removed, and use it from parent class
+    # (class UnstractFileSystem)
+    @staticmethod
+    def python_social_auth_backend() -> str:
+        return ""
+
     def test_credentials(self) -> bool:
         """To test credentials for SFTP."""
         is_dir = False
+        user_dir = f"{self.directory.strip('/')}/"
         try:
-            is_dir = bool(self.get_fsspec_fs().isdir("/"))
+            is_dir = bool(self.get_fsspec_fs().isdir(user_dir))
         except Exception as e:
             raise ConnectorError(
                 f"Error while connecting to SFTP server: {str(e)}"
@@ -79,3 +102,42 @@ class SftpFS(UnstractFileSystem):
                 "please check the connection settings."
             )
         return True
+
+    def create_dir_if_not_exists(self, input_dir: str) -> None:
+        """Method to create dir of a connector if not exists.
+
+        Args:
+            input_dir (str): input directory of source connector
+        """
+        try:
+            super().create_dir_if_not_exists(input_dir=input_dir)
+        except PermissionError as e:
+            self.raise_permission_error(input_dir=input_dir, error=e)
+
+    def upload_file_to_storage(self, source_path: str, destination_path: str) -> None:
+        """Method to upload filepath from tool to sftp connector directory.
+
+        Args:
+            source_path (str): local path of file to be uploaded, coming from tool
+            destination_path (str): target path in the storage where the file will be
+            uploaded
+        """
+        try:
+            normalized_path = os.path.normpath(destination_path)
+            super().upload_file_to_storage(
+                source_path=source_path, destination_path=destination_path
+            )
+        except PermissionError as e:
+            self.raise_permission_error(input_dir=normalized_path, error=e)
+
+    def raise_permission_error(
+        self, input_dir: str, error: PermissionError
+    ) -> PermissionDeniedError:
+        user_message = (
+            "Please verify your SFTP credentials and ensure you "
+            f" ensure you have the necessary permissions for the path '{input_dir}'. "
+        )
+        raise PermissionDeniedError(
+            user_message,
+            treat_as_user_message=True,
+        ) from error
