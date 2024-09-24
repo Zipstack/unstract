91d13c7656d3572609935da31bb0ec391213b113
ali <117142933+muhammad-ali-e@users.noreply.github.com>
2024-09-24 17:46:24 +0530

    schema migrations and settings for v2 applications (#536)
    * schema migrations and settings
    
    * renamed table name and one feild
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * changed env V2_SCHEMA to DB_SCHEMA
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- backend/api_v2/deployment_helper.py
+++ backend/api_v2/deployment_helper.py
@@ -191,6 +191,7 @@ class DeploymentHelper:
         api: APIDeployment,
         file_objs: list[UploadedFile],
         timeout: int,
+        include_metadata: bool = False,
     ) -> ReturnDict:
         """Execute workflow by api.
 
@@ -217,6 +218,7 @@ class DeploymentHelper:
                 hash_values_of_files=hash_values_of_files,
                 timeout=timeout,
                 execution_id=execution_id,
+                include_metadata=include_metadata,
             )
             result.status_api = DeploymentHelper.construct_status_endpoint(
                 api_endpoint=api.api_endpoint, execution_id=execution_id
