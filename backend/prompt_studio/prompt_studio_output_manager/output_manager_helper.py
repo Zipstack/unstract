9fa5bcdfcdff4af8ceb470f02984d8677aec6334
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2024-09-30 19:26:02 +0530

    Added todo to use enum
    Signed-off-by: jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>

--- backend/prompt_studio/prompt_studio_output_manager/output_manager_helper.py
+++ backend/prompt_studio/prompt_studio_output_manager/output_manager_helper.py
@@ -139,6 +139,7 @@ class OutputManagerHelper:
                 challenge_data["file_name"] = metadata.get("file_name")
 
             output = outputs.get(prompt.prompt_key)
+            # TODO: use enums here
             if prompt.enforce_type in {"json", "table", "record"}:
                 output = json.dumps(output)
             profile_manager = default_profile
