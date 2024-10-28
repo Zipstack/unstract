516fcbb511f752056ef0ae5673d35e5c706c56ea
Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
2024-10-28 17:17:04 +0530

    fix: Updated run-platform to create schema for new users (#820)
    * Updated run-platform to create schema for new users, existing users will have to run migrate-to-v2.sh script
    
    * Removed unnecessary code in script
    
    * Moved schema creation to entrypoint script, added a commands app for schema creation commands, minor run-platform.sh changes
    
    * Minor comment added to migrate-to-v2.sh script

---
+++ backend/commands/management/commands/drop_schema.py
@@ -0,0 +1,37 @@
+import os
+
+from django.core.management.base import BaseCommand
+from django.db import connection
+
+SCHEMA_NAME = os.getenv("DB_SCHEMA", None)
+
+
+class Command(BaseCommand):
+    help = (
+        "Drops schema if it exists. Relies on optional argument '--schema'"
+        "or env 'DB_SCHEMA' for the schema to drop"
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--schema",
+            type=str,
+            help=(
+                "Optional schema name to drop. Overrides env 'DB_SCHEMA' if specified"
+            ),
+        )
+
+    def handle(self, *args, **kwargs):
+        schema_name = kwargs["schema"] or os.getenv("DB_SCHEMA")
+
+        if not schema_name:
+            raise ValueError(
+                "No schema name provided. Set 'DB_SCHEMA' in the environment "
+                "or use '--schema' argument."
+            )
+
+        with connection.cursor() as cursor:
+            cursor.execute(f"DROP SCHEMA IF EXISTS {schema_name} CASCADE")
+        self.stdout.write(
+            self.style.SUCCESS(f'Schema "{schema_name}" dropped successfully.')
+        )
