bdce42f0e0f74596828067fb1171ca15647ab0f0
vishnuszipstack <117254672+vishnuszipstack@users.noreply.github.com>
2024-11-05 10:30:07 +0530

    Update frontend/src/components/custom-tools/pdf-viewer/PdfViewer.jsx
    Co-authored-by: Tahier Hussain <89440263+tahierhussain@users.noreply.github.com>
    Signed-off-by: vishnuszipstack <117254672+vishnuszipstack@users.noreply.github.com>

--- frontend/src/components/custom-tools/pdf-viewer/PdfViewer.jsx
+++ frontend/src/components/custom-tools/pdf-viewer/PdfViewer.jsx
@@ -22,8 +22,8 @@ function PdfViewer({ fileUrl, highlightData }) {
   const parentRef = useRef(null);
   function removeZerosAndDeleteIfAllZero(highlightData) {
     return highlightData.filter(
-      (innerArray) => !innerArray.every((value) => value === 0)
-    ); // Remove arrays where all values are 0
+      (innerArray) => innerArray.some((value) => value !== 0)
+    ); // Keep arrays that contain at least one non-zero value
   }
   let highlightPluginInstance = "";
   if (RenderHighlights && highlightData) {
