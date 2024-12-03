da8f692bb35b74e8519857bedaadd9e83d9c5453
Chandrasekharan M <117059509+chandrasekharan-zipstack@users.noreply.github.com>
2024-12-03 16:52:41 +0530

    feat: [OSS] Schema creation moved to DB init (#864)
    * Schema creation moved to DB init
    
    * Reverted change made for testing, minor README.md changes
    
    * Minor comment change
    
    * Removed unused code flow in backend entrypoint.sh
    
    ---------
    
    Co-authored-by: Hari John Kuriakose <hari@zipstack.com>

--- backend/init.sql
+++
@@ -1,6 +0,0 @@
-ALTER ROLE unstract_dev SET client_encoding TO 'utf8';
-ALTER ROLE unstract_dev SET default_transaction_isolation TO 'read committed';
-ALTER ROLE unstract_dev SET timezone TO 'UTC';
-ALTER USER unstract_dev CREATEDB;
-GRANT ALL PRIVILEGES ON DATABASE unstract_db TO unstract_dev;
-CREATE DATABASE unstract;
