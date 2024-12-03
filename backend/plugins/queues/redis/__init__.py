62e37de352f2136648f6446f6492d5278b47b742
kirtimanmishrazipstack <kirtiman.mishra@zipstack.com>
2024-12-03 11:49:33 +0530

    removing plugins

--- backend/plugins/queues/redis/__init__.py
+++
@@ -1,9 +0,0 @@
-from .redis_queue import RedisQueue
-
-metadata = {
-    "name": RedisQueue.__name__,
-    "version": "1.0.0",
-    "connector": RedisQueue,
-    "description": "Redis Queue connector",
-    "is_active": True,
-}
