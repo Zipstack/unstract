0c97509fc39f4b4f7547cf997aef5e568addbc17
vishnuszipstack <vishnu@zipstack.com>
2024-12-11 16:23:22 +0530

    added required key in prompt studio registry and removed some unwanted codes

--- prompt-service/src/unstract/prompt_service/helper.py
+++ prompt-service/src/unstract/prompt_service/helper.py
@@ -297,7 +297,6 @@ def run_completion(
         answer: str = completion[PSKeys.RESPONSE].text
         highlight_data = completion.get(PSKeys.HIGHLIGHT_DATA)
         confidence_data = completion.get(PSKeys.CONFIDENCE_DATA)
-        required_fields = True
         if metadata is not None and prompt_key:
             if highlight_data:
                 metadata.setdefault(PSKeys.HIGHLIGHT_DATA, {})[
@@ -308,9 +307,6 @@ def run_completion(
                 metadata.setdefault(PSKeys.CONFIDENCE_DATA, {})[
                     prompt_key
                 ] = confidence_data
-            metadata.setdefault(PSKeys.REQUIRED_FIELDS, {})[
-                prompt_key
-            ] = required_fields
         return answer
     # TODO: Catch and handle specific exception here
     except SdkRateLimitError as e:
@@ -362,5 +358,4 @@ def add_required_field(
     """
     if required:
         required_fields.append(key)
-
     return required_fields
