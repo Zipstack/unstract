2a45f4954fe76e9833f805c77037395639d2533e
harini-venkataraman <harini@zipstack.com>
2024-12-12 00:37:06 +0530

    Rolling out RC6 to OSS

--- backend/prompt_studio/prompt_studio_core_v2/views.py
+++ backend/prompt_studio/prompt_studio_core_v2/views.py
@@ -437,13 +437,21 @@ class PromptStudioCoreView(viewsets.ModelViewSet):
                     file_path += file_name
                 contents = FileManagerHelper.fetch_file_contents(file_system, file_path)
         else:
-            contents = PromptStudioFileHelper.fetch_file_contents(
-                file_name=file_name,
-                org_id=UserSessionUtils.get_organization_id(request),
-                user_id=custom_tool.created_by.user_id,
-                tool_id=str(custom_tool.tool_id),
-            )
-
+            try:
+                contents = PromptStudioFileHelper.fetch_file_contents(
+                    file_name=file_name,
+                    org_id=UserSessionUtils.get_organization_id(request),
+                    user_id=custom_tool.created_by.user_id,
+                    tool_id=str(custom_tool.tool_id),
+                )
+            except FileNotFoundError:
+                return Response(
+                    {
+                        "data": f"File {file_name} not found. Looks like it "
+                        "must be deleted or moved to a different location."
+                    },
+                    status=status.HTTP_404_NOT_FOUND,
+                )
         return Response({"data": contents}, status=status.HTTP_200_OK)
 
     @action(detail=True, methods=["post"])
