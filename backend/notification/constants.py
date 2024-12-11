628d4eb1de947ba6e459e0d6931fee2f41f95c60
ali <117142933+muhammad-ali-e@users.noreply.github.com>
2024-12-11 12:28:54 +0530

    cleaning up of some v1 applications (#879)

--- backend/notification/constants.py
+++
@@ -1,5 +0,0 @@
-class NotificationUrlConstant:
-    """Constants for Notification Urls."""
-
-    PIPELINE_UID = "pipeline_uuid"
-    API_UID = "api_uuid"
--- backend/usage/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Usage
-
-admin.site.register(Usage)
