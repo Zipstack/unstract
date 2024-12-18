7fea3e1ad6a28e72f43c5731222383abc82540aa
ali <117142933+muhammad-ali-e@users.noreply.github.com>
2024-12-18 11:57:34 +0530

    cleaning up of v1 applications (#902)

--- backend/workflow_manager/workflow/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .execution import WorkflowExecution  # noqa: F401
-from .execution_log import ExecutionLog  # noqa: F401
-from .file_history import FileHistory  # noqa: F401
-from .workflow import Workflow  # noqa: F401
