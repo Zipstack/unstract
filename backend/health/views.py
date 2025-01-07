875721173629260dd89f6345ff85c917cd1112fc
harini-venkataraman <harini@zipstack.com>
2025-01-07 09:52:12 +0530

    Fixing security hotspots

--- backend/health/views.py
+++ backend/health/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -8,6 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 @api_view(["GET"])
+@require_http_methods(["GET"])
 def health_check(request: Request) -> Response:
     logger.debug("Verifying backend health..")
     return Response(status=200)
