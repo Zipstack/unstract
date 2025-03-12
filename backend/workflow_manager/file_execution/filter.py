10f8c5cdd3e30b24bef9ca9cbdc4bb75f4d778b5
Chandrasekharan M <chandrasekharan@zipstack.com>
2025-03-12 12:55:07 +0530

    feat: Filter changes for file execution status to support multi choice Execution log filter support added for log level, file execution ID

--- backend/workflow_manager/file_execution/filter.py
+++ backend/workflow_manager/file_execution/filter.py
@@ -1,10 +1,15 @@
 import django_filters
 from django_filters import rest_framework as filters
-from workflow_manager.file_execution.models import WorkflowFileExecution
+from workflow_manager.file_execution.models import (
+    ExecutionStatus,
+    WorkflowFileExecution,
+)
 
 
 class FileExecutionFilter(filters.FilterSet):
-    status = django_filters.BaseInFilter(field_name="status", lookup_expr="in")
+    status = django_filters.MultipleChoiceFilter(
+        field_name="status", choices=ExecutionStatus.choices
+    )
     file_name = django_filters.CharFilter(
         field_name="file_name", lookup_expr="icontains"
     )
