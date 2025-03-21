59816769de7dc0ab21b0ba44753c961834e7a457
Ritwik G <100672805+ritwik-g@users.noreply.github.com>
2025-03-21 17:33:20 +0530

    Changed dev cors
    - Removed deprecated `CORS_ALLOWED_ORIGINS`
    - `CORS_ALLOW_METHODS` and `CORS_ALLOW_HEADERS` only need default values.
    
    Signed-off-by: Ritwik G <100672805+ritwik-g@users.noreply.github.com>

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -5,23 +5,10 @@ DEBUG = True
 X_FRAME_OPTIONS = "http://localhost:3000"
 X_FRAME_OPTIONS = "ALLOW-FROM http://localhost:3000"
 
-CORS_ALLOWED_ORIGINS = [
+# Add to the CORS_ALLOWED_ORIGINS from base
+CORS_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS + [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://frontend.unstract.localhost",
     # Other allowed origins if needed
-]
-
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://frontend.unstract.localhost",
-    # Other allowed origins if needed
-]
-
-CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
-
-CORS_ALLOW_HEADERS = [
-    "authorization",
-    "content-type",
 ]
