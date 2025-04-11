0bfc37f5b984b0ce0d2fd5d3add7a56b096f4134
harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
2025-04-11 21:01:18 +0530

    [FIX] Prompt service v2 fixes  (#1247)
    * Fix summary token issue
    
    * Fix summary token issue
    
    * Fixing line-item extractor
    
    * Fixing metrics
    
    * Token fixes
    
    * Fix metrics
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Removed unwanted logs
    
    ---------
    
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Deepak K <89829542+Deepak-Kesavan@users.noreply.github.com>
    Co-authored-by: ali <117142933+muhammad-ali-e@users.noreply.github.com>

--- backend/prompt_studio/prompt_studio_core_v2/prompt_studio_helper.py
+++ backend/prompt_studio/prompt_studio_core_v2/prompt_studio_helper.py
@@ -859,25 +859,6 @@ class PromptStudioHelper:
             doc_path = str(
                 doc_path.parent.parent / "summarize" / (doc_path.stem + ".txt")
             )
-            summarize_file_path = PromptStudioHelper.summarize(
-                filename, org_id, document_id, is_summary, run_id, tool, doc_id
-            )
-            summarize_doc_id = IndexingUtils.generate_index_key(
-                vector_db=str(default_profile.vector_store.id),
-                embedding=str(default_profile.embedding_model.id),
-                x2text=str(default_profile.x2text.id),
-                chunk_size=str(default_profile.chunk_size),
-                chunk_overlap=str(default_profile.chunk_overlap),
-                file_path=summarize_file_path,
-                fs=fs_instance,
-                tool=util,
-            )
-            PromptStudioIndexHelper.handle_index_manager(
-                document_id=document_id,
-                is_summary=is_summary,
-                profile_manager=profile_manager,
-                doc_id=summarize_doc_id,
-            )
             logger.info("Summary enabled, set chunk to zero..")
         logger.info(f"Indexing document {doc_path} for {doc_id}")
         index_result = PromptStudioHelper.dynamic_indexer(
