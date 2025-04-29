4dd9d0312a84fb2b01fdb758921220632a97d5b3
Jaseem Jas <jaseem@zipstack.com>
2025-04-29 17:48:46 +0530

    cleaned context removed

--- prompt-service/src/unstract/prompt_service/constants.py
+++ prompt-service/src/unstract/prompt_service/constants.py
@@ -66,7 +66,6 @@ class PromptServiceConstants:
     EPILOGUE = "epilogue"
     PLATFORM_POSTAMBLE = "platform_postamble"
     HIGHLIGHT_DATA_PLUGIN = "highlight-data"
-    CLEAN_CONTEXT = "clean-context"
     SUMMARIZE_AS_SOURCE = "summarize_as_source"
     VARIABLE_MAP = "variable_map"
     RECORD = "record"
