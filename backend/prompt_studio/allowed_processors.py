e3a6ebd4353c04abef2c424ca86f3397a15b0f19
kira-offgrid <kira@offgridsec.com>
2025-05-01 06:57:13 +0000

    fix: python.lang.security.audit.non-literal-import.non-literal-import--tmp-7b165fbc-5235-4250-ac45-5f7061608bfe-backend-prompt_studio-processor_loader.py

---
+++ backend/prompt_studio/allowed_processors.py
@@ -0,0 +1,9 @@
+# List of allowed processor modules that can be dynamically imported
+ALLOWED_PROCESSORS = [
+    "prompt_studio.processors.base_processor",
+    "prompt_studio.processors.llm_processor",
+    "prompt_studio.processors.prompt_processor",
+    "prompt_studio.processors.rag_processor",
+    "prompt_studio.processors.tool_processor",
+    # Add any other legitimate processor modules here
+]
