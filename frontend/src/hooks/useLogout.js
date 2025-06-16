68ca721f94424dc6a07c0dd43b6b49f720532ea8
jagadeeswaran-zipstack <jagadeeswaran@zipstack.com>
2025-06-16 09:57:04 +0530

    handled logout catch block

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -5,11 +5,15 @@ import { usePostHog } from "posthog-js/react";
 import { getSessionData } from "../helpers/GetSessionData";
 import { getBaseUrl } from "../helpers/GetStaticData";
 import { useSessionStore } from "../store/session-store";
+import { useAlertStore } from "../store/alert-store";
+import { useExceptionHandler } from "./useExceptionHandler";
 
 function useLogout() {
   const setSessionDetails = useSessionStore((state) => state.setSessionDetails);
   const setLogoutLoading = useSessionStore((state) => state.setLogoutLoading);
   const posthog = usePostHog();
+  const { setAlertDetails } = useAlertStore();
+  const handleException = useExceptionHandler();
 
   const clearSessionCookies = () => {
     Cookies.remove("sessionid");
@@ -25,7 +29,7 @@ function useLogout() {
     try {
       await axios.get(baseUrl + "/api/v1/logout");
     } catch (error) {
-      // ignore errors and proceed to navigation
+      setAlertDetails(handleException(error));
     }
     setLogoutLoading(false);
     window.location.href = baseUrl + "/";
