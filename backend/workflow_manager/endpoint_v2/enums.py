e21f2b19a7f19e501266447328ada0a408273761
ali <117142933+muhammad-ali-e@users.noreply.github.com>
2025-06-23 14:07:06 +0530

    UN-2445 [FEAT] Prevent unsupported file in API and ETL workflow-executions by validating file extension and MIME type (#1361)
    * UN-2445 Skip to process unsupported file formats
    
    * updated with FE logs, Added fix on filehash
    
    ---------
    
    Co-authored-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>

---
+++ backend/workflow_manager/endpoint_v2/enums.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class AllowedFileTypes(Enum):
+    PLAIN_TEXT = "text/plain"
+    PDF = "application/pdf"
+    JPEG = "image/jpeg"
+    PNG = "image/png"
+    TIFF = "image/tiff"
+    BMP = "image/bmp"
+    GIF = "image/gif"
+    WEBP = "image/webp"
+    DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    DOC = "application/msword"
+    XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    XLS = "application/vnd.ms-excel"
+    PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    PPT = "application/vnd.ms-powerpoint"
+    ODT = "application/vnd.oasis.opendocument.text"
+    ODS = "application/vnd.oasis.opendocument.spreadsheet"
+    ODP = "application/vnd.oasis.opendocument.presentation"
+    CDFV2 = "application/CDFV2"
+    JSON = "application/json"
+    CSV = "text/csv"
+
+    @classmethod
+    def is_allowed(cls, mime_type: str) -> bool:
+        return mime_type in cls._value2member_map_
