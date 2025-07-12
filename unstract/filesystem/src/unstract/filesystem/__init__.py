700c89e9345521d94dfed0554cbefac6479f9014
Hari John Kuriakose <hari@zipstack.com>
2025-07-12 15:38:27 +0530

    [feat] integrate sdk1 with filesystem core package

--- unstract/filesystem/src/unstract/filesystem/__init__.py
+++ unstract/filesystem/src/unstract/filesystem/__init__.py
@@ -1,8 +1,13 @@
 """This module exposes the main classes for file storage handling."""
 
-from unstract.sdk.file_storage import SharedTemporaryFileStorage
+from unstract.flags.feature_flag import check_feature_flag_status
 
 from .file_storage_types import FileStorageType
 from .filesystem import FileSystem
+
+if check_feature_flag_status("sdk1"):
+    from unstract.sdk1.file_storage import SharedTemporaryFileStorage
+else:
+    from unstract.sdk.file_storage import SharedTemporaryFileStorage
 
 __all__ = ["FileSystem", "SharedTemporaryFileStorage", "FileStorageType"]
