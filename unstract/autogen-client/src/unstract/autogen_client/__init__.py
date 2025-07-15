685ede40283b222aafa9c1281d9af852d369b642
Deepak <89829542+Deepak-Kesavan@users.noreply.github.com>
2025-07-15 10:44:38 +0530

    UN-2563 Initial changes for unstract autogen client
    Signed-off-by: Deepak <89829542+Deepak-Kesavan@users.noreply.github.com>

---
+++ unstract/autogen-client/src/unstract/autogen_client/__init__.py
@@ -0,0 +1,21 @@
+"""Unstract AutoGen Client Package.
+
+A Python package that provides a ChatCompletionClient implementation for Microsoft AutoGen framework,
+using Unstract LLM adapters as the backend for LLM interactions.
+"""
+
+__version__ = "0.1.0"
+
+from .client import UnstractAutoGenClient
+from .exceptions import (
+    UnstractAutoGenError,
+    UnstractCompletionError,
+    UnstractConfigurationError,
+)
+
+__all__ = [
+    "UnstractAutoGenClient",
+    "UnstractAutoGenError",
+    "UnstractConfigurationError",
+    "UnstractCompletionError",
+]
