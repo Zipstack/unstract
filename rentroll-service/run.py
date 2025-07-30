b8e1b400e907f39c308ff405962123ee32d34d96
harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>
2025-07-30 15:56:02 +0530

    Delete rentroll-service directory
    Signed-off-by: harini-venkataraman <115449948+harini-venkataraman@users.noreply.github.com>

--- rentroll-service/run.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-from unstract.rentroll_service.app import app
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    app.run(host="0.0.0.0", port=5003, debug=True)
--- rentroll-service/src/unstract/rentroll_service/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rent_roll_locator import RentRollLocatorAgent
-from .rent_roll_mapper import RentRollMapper
-
-__all__ = ["RentRollLocatorAgent", "RentRollMapper"]
