ba08db23ce09c18031dd4b69d35bdb720f9d3fbe
pk-zipstack <praveen@zipstack.com>
2025-09-25 15:40:08 +0530

    Revert "- Removed .gitignore .pre-commit-config.yaml"
    This reverts commit 8a2ed67ef8f4e7134fb7eb735b0dac40be67ebf1.

--- backend/plugins/configuration/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Cloud configuration plugin initialization."""
-
-# Metadata for the configuration plugin
-metadata = {
-    "name": "CloudConfiguration",
-    "is_active": True,
-    "description": "Cloud-specific configuration extensions",
-}
