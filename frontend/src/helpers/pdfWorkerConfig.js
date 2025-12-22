fced57f55fe71c1b0e4eadc1dc014e43e9b0ae03
Jaseem Jas <jaseem@zipstack.com>
2025-12-22 22:31:10 +0530

    refactor: Use export...from syntax for re-exporting PDF worker URL
    Addresses SonarCloud code smell by using the more concise
    'export { default as X } from' syntax instead of separate
    import and export statements.

--- frontend/src/helpers/pdfWorkerConfig.js
+++ frontend/src/helpers/pdfWorkerConfig.js
@@ -1,5 +1,3 @@
 // Load the worker file from the installed pdfjs-dist package
 // The ?url suffix tells Webpack 5 to return the URL of the asset
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.min.js?url";
-
-export const PDF_WORKER_URL = pdfjsWorker;
+export { default as PDF_WORKER_URL } from "pdfjs-dist/build/pdf.worker.min.js?url";
