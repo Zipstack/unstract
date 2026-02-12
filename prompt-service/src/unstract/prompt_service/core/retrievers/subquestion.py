5fc3ef883342387087539b16f5b186213b3f7c1e
pk-zipstack <praveen@zipstack.com>
2026-02-13 01:15:46 +0530

    Fixed import error with sub-question retrieval

--- prompt-service/src/unstract/prompt_service/core/retrievers/subquestion.py
+++ prompt-service/src/unstract/prompt_service/core/retrievers/subquestion.py
@@ -1,6 +1,7 @@
 import logging
 
 from llama_index.core.query_engine import SubQuestionQueryEngine
+from llama_index.core.question_gen.llm_generators import LLMQuestionGenerator
 from llama_index.core.schema import QueryBundle
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from unstract.prompt_service.core.retrievers.base_retriever import BaseRetriever
@@ -38,8 +39,12 @@ class SubquestionRetriever(BaseRetriever):
             ]
             query_bundle = QueryBundle(query_str=self.prompt)
 
+            question_gen = LLMQuestionGenerator.from_defaults(
+                llm=self.llama_index_llm,
+            )
             query_engine = SubQuestionQueryEngine.from_defaults(
                 query_engine_tools=query_engine_tools,
+                question_gen=question_gen,
                 use_async=True,
                 llm=self.llama_index_llm,
             )
