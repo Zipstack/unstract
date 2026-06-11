c27d32610ff667112bceea0bcb9d72ea5d4d4c7f
Kirtiman Mishra <110175055+kirtimanmishrazipstack@users.noreply.github.com>
2026-06-11 10:05:18 +0530

    UN-3166 [FIX] Use group-shared adapters in Prompt Studio & workflows (#2028)
    shared adapters can be run in PS + woprkflow

--- backend/tool_instance_v2/tool_instance_helper.py
+++ backend/tool_instance_v2/tool_instance_helper.py
@@ -9,6 +9,7 @@ from adapter_processor_v2.models import AdapterInstance
 from django.core.exceptions import PermissionDenied
 from django.core.exceptions import ValidationError as DjangoValidationError
 from jsonschema.exceptions import ValidationError as JSONValidationError
+from permissions.permission import has_group_access
 from prompt_studio.prompt_studio_registry_v2.models import PromptStudioRegistry
 from tenant_account_v2.organization_member_service import OrganizationMemberService
 from workflow_manager.workflow_v2.constants import WorkflowKey
@@ -508,6 +509,7 @@ class ToolInstanceHelper:
                 or adapter_instance.shared_to_org
                 or adapter_instance.created_by == user
                 or adapter_instance.shared_users.filter(pk=user.pk).exists()
+                or has_group_access(user, adapter_instance)
             ):
                 logger.error(
                     "User %s doesn't have access to adapter %s",
